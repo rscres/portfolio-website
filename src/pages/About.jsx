93b99b892b305752b21a7a38cc831a6deb765249
rscres <renatocrescenti@gmail.com>
2024-03-25 15:31:24 -0300

    finish about page layout, need to sub skills words for images

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,39 +3,97 @@ import './About.css'
 function About() {
   return (
     <>
-      <section className='about-container'>
-        <h1 className='about-title'>About Me</h1>
-        <ul className='links'>
-          <li><a href='https://www.linkedin.com/in/renato-crescenti/' target='_blank' rel='noreferrer'>LinkedIn</a></li>
-          <li><a href='https://github.com/rscres' target='_blank' rel='noreferrer'>GitHub</a></li>
+    <section className='about-container'>
+      <h1 className='about-title'>About Me</h1>
+      <ul className='links'>
+        <li><a href='https://www.linkedin.com/in/renato-crescenti/' target='_blank' rel='noreferrer'>LinkedIn</a></li>
+        <li><a href='https://github.com/rscres' target='_blank' rel='noreferrer'>GitHub</a></li>
+        <li><a href='./assets/BG.jpg' download>CV</a></li>
+      </ul>
+      <p className='about-text'>Hello, my name is Renato Crescenti and I'm a web developer/designer based in Brazil. I specialize in creating beautiful, responsive websites and applications for businesses and individuals.</p>
+      <section className='about-skills'>
+        <h2>Skills</h2>
+        <ul className='list-skills'>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Ruby</li>
+          <li>C</li>
         </ul>
-        <p className='about-text'>Hello, my name is Renato Crescenti and I'm a web developer/designer based in Brazil. I specialize in creating beautiful, responsive websites and applications for businesses and individuals.</p>
-        <section className='about-skills'>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
-          </ul>
-        </section>
-        <div className='list-separator'></div>
-        <section className='about-education'>
-          <ul>
-            <li>42SP</li>
-            <li>Gobelins</li>
-            <li>ROTC</li>
-            <li>FAAP</li>
-          </ul>
-        </section>
-        <div className='list-separator'></div>
-        <section className='about-experience'>
-          <ul>
-            <li>TNZPV</li>
-            <li>LEGO</li>
-            <li>VetorZero</li>
-          </ul>
-        </section>
       </section>
+      <div className='list-separator'></div>
+      <section className='about-education'>
+        <h2>Education</h2>
+        <ul>
+          <ListItem
+            name='42SP'
+            location='Sao Paulo, Brazil'
+            year='2023-24'
+            text='Software Engeneering Course'
+          />
+          <ListItem
+            name='Gobelins'
+            location='Annecy, France'
+            year='2017-18'
+            text='3D Character Animation Course'
+          />
+          <ListItem
+            name='FAAP'
+            location='Sao Paulo, Brazil'
+            year='2010-15'
+            text="Bachelor in Industrial Design"
+          />
+          <ListItem
+            name='Brazilian Army ROTC'
+            location='Sao Paulo, Brazil'
+            year='2011'
+            text='Reserve Officer Training Course'
+          />
+        </ul>
+      </section>
+      <div className='list-separator'></div>
+      <section className='about-experience'>
+        <h2>Work</h2>
+        <ul>
+        <ListItem
+            name='TNZPV'
+            location='Arles, France'
+            year='2018-19'
+            text='Junior 3D Animator'
+          />
+          <ListItem
+            name='The LEGO Group'
+            location='London, UK'
+            year='2016-17'
+            text='Sales Associate and Stocking Assistant'
+          />
+          <ListItem
+            name='VetorZero'
+            location='Sao Paulo, Brazil'
+            year='2015-16'
+            text='3D Animator'
+          />
+        </ul>
+      </section>
+    </section>
+    </>
+  )
+}
+
+function ListItem({ ...props }) {
+  return (
+    <>
+    <li>
+      <h3 className='item-title'>{props.name}</h3>
+      <div className='item-info'>
+        <p>{props.location}</p>
+        <p>{props.year}</p>
+      </div>
+      <div className='item-body'>
+        <p className='item-text'>{props.text}</p>
+      </div>
+    </li>
     </>
   )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,8 +2,10 @@ import './Contact.css'
 
 function Contact() {
   return (
-    <div className='contact-container'>
-      <form className='contact-form'>
+    <>
+    <section className='contact-container'>
+      <h1>Contact</h1>
+      {/* <form className='contact-form'>
         <label>Name</label>
         <input type='text' required/>
         <label>Email</label>
@@ -11,14 +13,25 @@ function Contact() {
         <label>Message</label>
         <textarea type='textarea' rows='5' className='message-form'required/>
         <button type='submit'>Submit</button>
-      </form>
-      {/* <form className='contact-form' action="https://formsubmit.co/renatocrescenti@gmail.com" method="POST">
-        <input type="text" name="name" required/>
-        <input type="email" name="email" required/>
-        <input type='text' name='textarea' className='message-form' required/>
-        <button type="submit">Send</button>
       </form> */}
-    </div>
+      <form className='contact-form' action="https://formsubmit.co/renatocrescenti@gmail.com" method="POST">
+        <CustomInput tag='input' label='Name' type='text' name='name'/>
+        <CustomInput tag='input' label='Email' type='email' name='email'/>
+        <label>Message</label>
+        <textarea type='text' name='message' rows='8' className='message-form' required/>
+        <button type="submit">Send</button>
+      </form>
+    </section>
+    </>
+  )
+}
+
+function CustomInput({ tag: Tag, ...props }) {
+  return (
+    <>
+      <label>{props.label}</label>
+      <Tag type={props.type} name={props.name} required/>
+    </>
   )
 }
 
