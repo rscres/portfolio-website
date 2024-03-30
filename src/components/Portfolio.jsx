0a23267ad85a9bc9979e1021ae318f1c06ec15c2
rscres <renatocrescenti@gmail.com>
2024-03-30 13:25:58 -0300

    add modal to portfolio projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,3 +1,6 @@
+import { useState }from "react";
+import { createPortal } from "react-dom";
+
 function PortfolioSection({ projects }) {
 
   return (
@@ -10,17 +13,46 @@ function PortfolioSection({ projects }) {
 }
 
 function ProjectCard({ ...props }) {
+  const [isOverlay, setIsOverlay] = useState(false);
+
+  function toggleOverlay() {
+    setIsOverlay(!isOverlay);
+  }
+
   return (
-    <div className='project-container'>
-    <h2 className='project-name'>{props.name}</h2>
-    <a href={props.link} target='_blank' rel='noreferrer'>
-        <img src={props.image} alt={props.alt} className='project-img'></img>
-    </a>
-    <div className='project-description'>
-        <p className='project-text'>{props.text}</p>
-        <p>{props.stack}</p>
+    <>
+      <div className='project-container' onClick={toggleOverlay}>
+        <h2 className='project-name'>{props.name}</h2>
+        <img src={props.image} alt={props.alt} className='project-img'/>
+        <div className='project-description'>
+            <p className='project-text'>{props.text}</p>
+            <p>{props.stack}</p>
+        </div>
+      </div>
+      {isOverlay && createPortal(
+        <ProjectOverlay {...props} action={toggleOverlay}/>,
+        document.body)}
+    </>
+  )
+}
+
+function ProjectOverlay({ action, ...props }) {
+  return (
+    <>
+    <div className='overlay'onClick={action}/>
+    <div className='overlay-container'>
+      <div className='overlay-content'>
+        <h2 className='project-name'>{props.name}</h2>
+        <a href={props.link} target='_blank' rel='noreferrer'>
+          <img src={props.image} alt={props.alt} className='project-img'/>
+        </a>
+        <div className='project-description'>
+          <p className='project-text'>{props.text}</p>
+          <p>{props.stack}</p>
+        </div>
+      </div>
     </div>
-    </div>
+    </>
   )
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,28 +8,28 @@ function About() {
       <div className='about-content'>
         <ul className='links'>
           <li><a href='https://www.linkedin.com/in/renato-crescenti/' target='_blank' rel='noreferrer'>
-            <img src='/assets/linkedin.png' alt='Linkedin logo'></img></a>
+            <img src='/assets/linkedin.png' alt='Linkedin logo'/></a>
           </li>
           <li><a href='https://github.com/rscres' target='_blank' rel='noreferrer'>
-            <img src='/assets/github.png' alt='Github logo'></img></a>
+            <img src='/assets/github.png' alt='Github logo'/></a>
           </li>
           <li><a href='./assets/BG.jpg' download>
-            <img src='/assets/cv.png' alt='cv icon'></img></a>
+            <img src='/assets/cv.png' alt='cv icon'/></a>
           </li>
         </ul>
         <p className='about-text'>Hello, my name is Renato Crescenti and I'm a web developer/designer based in Brazil. I specialize in creating beautiful, responsive websites and applications for businesses and individuals.</p>
         <section className='about-skills'>
           <h2>Skills</h2>
           <ul className='list-skills'>
-            <li><img src='/assets/html.png' alt='html5 logo'></img></li>
-            <li><img src='/assets/css.png' alt='css logo'></img></li>
-            <li><img src='/assets/js.png' alt='javascript logo'></img></li>
-            <li><img src='/assets/react.png' alt='react logo'></img></li>
-            <li><img src='/assets/ruby.png' alt='ruby logo'></img></li>
-            <li><img src='/assets/c.png' alt='c logo'></img></li>
+            <li><img src='/assets/html.png' alt='html5 logo'/></li>
+            <li><img src='/assets/css.png' alt='css logo'/></li>
+            <li><img src='/assets/js.png' alt='javascript logo'/></li>
+            <li><img src='/assets/react.png' alt='react logo'/></li>
+            <li><img src='/assets/ruby.png' alt='ruby logo'/></li>
+            <li><img src='/assets/c.png' alt='c logo'/></li>
           </ul>
         </section>
-        <div className='list-separator'></div>
+        <div className='list-separator'/>
         <section className='about-education'>
           <h2>Education</h2>
           <ul>
@@ -59,7 +59,7 @@ function About() {
             />
           </ul>
         </section>
-        <div className='list-separator'></div>
+        <div className='list-separator'/>
         <section className='about-experience'>
           <h2>Work</h2>
           <ul>
