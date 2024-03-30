7d005cb0a1148e2b173179e9bbf081c1ef13f0b1
rscres <renatocrescenti@gmail.com>
2024-03-29 22:19:03 -0300

    add responsive styles to all | Dev: add project images and make it links | Header: add hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,89 @@
-import { Link, useMatch, useResolvedPath } from 'react-router-dom'
-import '../App.css'
+import { useState } from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+import '../App.css';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-      <>
+    <>
       <header className='header-container'>
-          <Link to='/' className='header-name'>Renato<br/>Crescenti</Link>
-            <section className='header-portfolios'>
-              <ul>
-                <CustomLinks to='/dev'>Development</CustomLinks>
-                <CustomLinks to='/design'>Design</CustomLinks>
-              </ul>
-            </section>
-            <section className='header-about'>
-              <ul>
-                <CustomLinks to='/about'>About me</CustomLinks>
-                <CustomLinks to='/contact'>Contact</CustomLinks>
-              </ul>
-            </section>
+        <Link to='/' className='header-name' onClick={() => setIsMenuOpen(false)}>Renato<br/>Crescenti</Link>
+        <section className='header-portfolios'>
+          <ul>
+            <CustomLinks to='/dev'>Development</CustomLinks>
+            <CustomLinks to='/design'>Design</CustomLinks>
+          </ul>
+        </section>
+        <section className='header-about'>
+          <ul>
+            <CustomLinks to='/about'>About me</CustomLinks>
+            <CustomLinks to='/contact'>Contact</CustomLinks>
+          </ul>
+        </section>
+        <nav className='header-menu'>
+          <button className='menu-button' onClick={toggleMenu}>
+            <img src='menu.svg' alt='Menu' />
+          </button>
+        </nav>
       </header>
-      </>
-  )
+      {isMenuOpen && <Menu toggleMenu={toggleMenu}/>}
+    </>
+  );
 }
 
-function CustomLinks({ to, children, ...props}) {
-  const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+function Menu({ toggleMenu }) {
+  return (
+    <nav className='mobile-menu'>
+      <ul>
+        <CustomLinks to='/dev' action={toggleMenu}>Development</CustomLinks>
+        <CustomLinks to='/design' action={toggleMenu}>Design</CustomLinks>
+        <CustomLinks to='/about' action={toggleMenu}>About</CustomLinks>
+        <CustomLinks to='/contact' action={toggleMenu}>Contact</CustomLinks>
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLinks({ to, children, action}) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
   return (
     <li className={isActive ? 'active' : ''}>
-      <Link to={to}>{children}</Link>
+      <Link to={to} onClick={action}>{children}</Link>
     </li>
-  )
+  );
 }
 
-export default Header
+export default Header;
+
+
+// function Menu() {
+//   return (
+//     <nav>
+//       <ul>
+//         <CustomLinks to='/dev'>Development</CustomLinks>
+//         <CustomLinks to='/design'>Design</CustomLinks>
+//         <CustomLinks to='/about'>About</CustomLinks>
+//         <CustomLinks to='/contact'>Contact</CustomLinks>
+//       </ul>
+//     </nav>
+//   )
+// }
+
+// function CustomLinks({ to, children, ...props}) {
+//   const resolvedPath = useResolvedPath(to)
+//   const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+//   return (
+//     <li className={isActive ? 'active' : ''}>
+//       <Link to={to}>{children}</Link>
+//     </li>
+//   )
+// }
+
+// export default Header
--- src/pages/Dev.jsx
+++ src/pages/Dev.jsx
@@ -8,16 +8,24 @@ function Dev() {
       <section className='projects'>
         <ProjectCard 
           name='Portfolio website'
+          link='https://github.com/rscres/portfolio-website'
+          image='../assets/portfolio-website.png'
+          alt='Portfolio website'
           text='The quick brown fox jumps over the lazy dog. Lorem ipsum'
           stack='HTML, CSS, React'
           />
         <ProjectCard
           name='FdF'
+          link='https://github.com/rscres/42FdF'
+          image='../assets/FranceXXL.gif'
+          alt='FdF'
           text='Graphic project for 42 cursus'
           stack='C'
           />
         <ProjectCard
           name='Weather app'
+          image='../assets/BG.jpg'
+          alt='Weather app'
           text='React web app'
           stack='HTML, CSS, React'
           />
@@ -31,8 +39,9 @@ function ProjectCard({ ...props }) {
   return (
     <div className='project-container'>
       <h2 className='project-name'>{props.name}</h2>
-      <div className='project-img'>image</div>
-      {/* <img src='../assets/BG.jpg' className='project-img'></img> */}
+      <a href={props.link} target='_blank' rel='noreferrer'>
+        <img src={props.image} alt={props.alt} className='project-img'></img>
+      </a>
       <div className='project-description'>
         <p className='project-text'>{props.text}</p>
         <p>{props.stack}</p>
