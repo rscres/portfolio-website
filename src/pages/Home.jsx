4b804872e12027470f99c245fe1e65581851993e
rscres <renatocrescenti@gmail.com>
2024-07-31 08:25:19 -0300

    remove border radius from home buttons, add hover effect to porfolio cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,9 @@ function Home() {
       <h1 className='hero'>Software Engineer && Product designer</h1>
       <p className='hero-text'>Hello, my name is Renato Crescenti and I'm a developer/product designer based in Brazil. I specialize in creating beautiful, responsive websites and applications for businesses and individuals.</p>
       <div className="button-container">
-        <Link to='/contact' className='contact-button'>Contact Me</Link>
-        <Link to='/design' className='contact-button'>Design</Link>
-        <Link to='/dev' className='contact-button'>Development</Link>
+        <Link to='/contact' className='contact-button home-button'>Contact Me</Link>
+        <Link to='/design' className='contact-button home-button'>Design</Link>
+        <Link to='/dev' className='contact-button home-button'>Development</Link>
       </div>
     </section>
   )
